f9b259f587b43c5ba1efb829e0d74ec2986d6392
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-11-12 10:50:28 -0800

    wip

--- demoproject/workflows/driver_workflow.py
+++ demoproject/workflows/driver_workflow.py
@@ -1,19 +1,19 @@
-
 import ujson
-from flytekit.sdk.workflow import workflow_class, Output, Input
-from flytekit.sdk.types import Types
 from flytekit.sdk.tasks import python_task, inputs, outputs
-from flytekit.common import utils as flytekit_utils
-
-from workflows.classifier_train_workflow import train_lp, DEFAULT_VALIDATION_DATA_RATIO, DEFAULT_TRAINING_VALIDATION_CONFIG_FILE
+from flytekit.sdk.types import Types
+from flytekit.sdk.workflow import workflow_class, Output, Input
 from workflows.classifier_evaluate_workflow import evaluate_lp
+from workflows.classifier_train_workflow import train_lp, DEFAULT_VALIDATION_DATA_RATIO, \
+    DEFAULT_TRAINING_VALIDATION_CONFIG_FILE
 from workflows.data_preparation_workflow import data_prep
+
 
 @inputs(models=[Types.Blob])
 @outputs(second=Types.Blob)
 @python_task
 def pick_second(wf_params, models, second):
-	second.set(models[1])
+    second.set(models[1])
+
 
 @workflow_class
 class DriverWorkflow:
@@ -22,17 +22,18 @@ class DriverWorkflow:
     stream_extension = Input(Types.String, default="avi")
 
     streams_metadata_path = Input(Types.String, required=True)
-    training_validation_config_json = Input(Types.Generic, default=ujson.loads(open(DEFAULT_TRAINING_VALIDATION_CONFIG_FILE).read()))
+    training_validation_config_json = Input(Types.Generic,
+                                            default=ujson.loads(open(DEFAULT_TRAINING_VALIDATION_CONFIG_FILE).read()))
     validation_data_ratio = Input(Types.Float, default=DEFAULT_VALIDATION_DATA_RATIO)
 
     prepare = data_prep(
-    	streams_external_storage_prefix=streams_external_storage_prefix,
+        streams_external_storage_prefix=streams_external_storage_prefix,
         streams_names=streams_names,
         stream_extension=stream_extension)
 
     train = train_lp(
-    	available_streams_names=prepare.outputs.streams_names_out,
-    	available_streams_mpblobs=prepare.outputs.selected_frames_mpblobs,
+        available_streams_names=prepare.outputs.streams_names_out,
+        available_streams_mpblobs=prepare.outputs.selected_frames_mpblobs,
         streams_metadata_path=streams_metadata_path,
         training_validation_config_json=training_validation_config_json,
         validation_data_ratio=validation_data_ratio
@@ -44,7 +45,7 @@ class DriverWorkflow:
         streams_metadata_path=streams_metadata_path,
         evaluation_config_json=training_validation_config_json,
         model=pick_second.outputs.second
-	)
+    )
 
     ground_truths = Output(evaluate.outputs.ground_truths, sdk_type=[Types.Integer])
     predictions = Output(evaluate.outputs.predictions, sdk_type=[Types.Integer])
